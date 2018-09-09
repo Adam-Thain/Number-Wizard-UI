224e1cba01f4b9bd1e37140c9206b452581860f7
Adam-Thain <Adam.r.thain@gmail.com>
2018-09-09 18:10:35 +0100

    Added Initial Source Code
    Added all up to date Number Wizard code

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    
+    /// <summary>
+    /// Load scene
+    /// </summary>
+    /// <param name="name">The name of the level to load</param>
+    public void LoadLevel(string name){
+        SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Quit the game
+    /// </summary>
+    public void QuitRequest(){
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Go back to a specified location
+    /// </summary>
+    /// <param name="name">The name of the scene to go back to</param>
+    public void Back(string name){
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Number Wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+    int min;
+    int max;
+    int guess;
+
+    public int maxGuessesAllowed = 5;
+    public Text text;
+
+    /// <summary>
+    /// Start the game
+    /// </summary>
+	void Start (){
+        StartGame();
+	}
+
+    /// <summary>
+    /// Set variables and call first guess
+    /// </summary>
+    void StartGame(){
+        max = 1000;
+        min = 1;
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Make a lower guess
+    /// </summary>
+    public void GuessLower(){
+        max = guess;
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Make a higher guess
+    /// </summary>
+    public void GuessHigher(){
+        min = guess;
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Guess the mext number
+    /// </summary>
+    void NextGuess(){
+        guess = Random.Range(min, max + 1);
+        text.text = guess.ToString(); ;
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+
+        // if the number of guess becomes less than or equal to zero
+        if (maxGuessesAllowed <= 0){
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
